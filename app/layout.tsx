0e9579a768741129fcff11d5fec0ec07e757bf6c
naitikSahu16 <naitikop1616@gmail.com>
2026-08-03 20:45:33 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,21 @@
+import { ClerkProvider } from '@clerk/nextjs'
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <head>
-        {/* THE NUCLEAR FIX: FORCING TAILWIND CSS DIRECTLY IN BROWSER */}
-        <script src="https://cdn.tailwindcss.com"></script>
-      </head>
-      <body className="bg-[#020614] text-white m-0 p-0 overflow-x-hidden">
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          {/* THE NUCLEAR FIX: FORCING TAILWIND CSS DIRECTLY IN BROWSER */}
+          <script src="https://cdn.tailwindcss.com"></script>
+        </head>
+        <body className="bg-[#020614] text-white m-0 p-0 overflow-x-hidden">
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
